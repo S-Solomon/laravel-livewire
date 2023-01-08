3f6f4df3e9c82bd19869e9b7a69a35880360c10f
Samuel Solomon <mrsamuelsolomon@gmail.com>
2023-01-08 21:07:02 +0000

    addComments only when the form is submitted

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -23,9 +23,9 @@
     </section> --}}
 
     <form class="my-4 flex" wire:submit.prevent="addComment">
-        <input type="text" class="w-full rounded border shadow p-2 mr-2 my-2" placeholder="What's in your mind" wire:model="newComment">
+        <input type="text" class="w-full rounded border shadow p-2 mr-2 my-2" placeholder="What's in your mind" wire:model.debounce.500ms="newComment">
         <div class="py-2">
-            <button type="submit" class="p-2 bg-blue-500 w-20 rounded shadow text-white" wire:click="addComment">
+            <button type="submit" class="p-2 bg-blue-500 w-20 rounded shadow text-white">
                 Add
             </button>
         </div>
